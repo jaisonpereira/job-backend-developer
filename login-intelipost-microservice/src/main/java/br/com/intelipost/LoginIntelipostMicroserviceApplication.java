74305d41c8432ed3a3a82bfb277011983a6d52c8
Jaison Pereira <jaisonpereira.infotech@gmail.com>
2018-06-14 23:04:55 -0300

    Implementacao Spring-cache + Spring-data-redis

--- login-intelipost-microservice/src/main/java/br/com/intelipost/LoginIntelipostMicroserviceApplication.java
+++ login-intelipost-microservice/src/main/java/br/com/intelipost/LoginIntelipostMicroserviceApplication.java
@@ -2,11 +2,14 @@ package br.com.intelipost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+
 //TODO HABILITAR EUREKA CLIENT
 //@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = "br.com.intelipost")
+@EnableCaching
 public class LoginIntelipostMicroserviceApplication {
 
 	public static void main(String[] args) {
--- login-intelipost-microservice/src/main/java/br/com/intelipost/repository/UserIntelipostRepositoryImpl.java
+++ login-intelipost-microservice/src/main/java/br/com/intelipost/repository/UserIntelipostRepositoryImpl.java
@@ -1,23 +1,30 @@
 package br.com.intelipost.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import br.com.intelipost.domain.UserIntelipost;
 
+@CacheConfig(cacheNames = "users_cache")
 public class UserIntelipostRepositoryImpl implements UserIntelipostRepositoryCustom {
 
+	private static final Logger logger = LoggerFactory.getLogger(UserIntelipostRepositoryImpl.class);
 	private static final String QUERY_CONSULTA_USER = "select id,password,name,email from tb_user_intelipost  where email= ?";
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	@Override
+	@Cacheable
 	public UserIntelipost getUserByEmail(String email) {
+		logger.trace("Cache enabled , Exibição de trace unico");
 		return this.jdbcTemplate.query(QUERY_CONSULTA_USER, new Object[] { email },
 				rs -> (rs.next()) ? new UserIntelipost(rs.getLong("id"), rs.getString("password"), rs.getString("name"),
 						rs.getString("email")) : null);
-
 	}
 
 }
