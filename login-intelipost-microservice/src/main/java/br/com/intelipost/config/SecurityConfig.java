64a81d7d5480ae35c27417ad96a435170d8b2d39
Jaison Pereira <jaisonpereira.infotech@gmail.com>
2018-06-16 19:55:54 -0300

    Bug ,properties jwt not found

--- login-intelipost-microservice/src/main/java/br/com/intelipost/config/SecurityConfig.java
+++ login-intelipost-microservice/src/main/java/br/com/intelipost/config/SecurityConfig.java
@@ -34,13 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS_GET = { "/**" };
 
-	private static final String[] PUBLIC_MATCHERS_POST = { "/login" };
+	private static final String[] PUBLIC_MATCHERS_POST = { "/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().authenticated();
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().permitAll();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
