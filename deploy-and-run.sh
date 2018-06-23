995747831cf44c1169d9958062d3db6bd94440e8
Jaison Pereira <jaison_pereira@hotmail.com>
2018-06-22 21:32:45 -0300

    corrigindo deploy-and-run.sh

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -3,5 +3,6 @@ cd eureka-server/
 cd ../zuul-gateway
 ./mvnw clean install  dockerfile:build
 cd ../login-intelipost-microservice
+./mvnw clean install  dockerfile:build
 cd ..
 docker-compose -f docker-compose-up-aplication.yml up
